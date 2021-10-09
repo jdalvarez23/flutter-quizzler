264eb22b400b4a84fd50a956bb0fa4ee3b3bfdbe
Jose D. Alvarez <jdalvarez23@gmail.com>
2021-10-09 09:20:53 -0500

    Added a Question class that contains the question and answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +72,20 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+
+                bool correctAnswer = questions[questionNumber].questionAnswer;
+
+                if (correctAnswer) {
+                  print('user got it right');
+                }
+
+                setState(() {
+                  questionNumber++;
+                });
+                // check if the question number reaches the end of the list
+                if (questionNumber == questions.length) {
+                  questionNumber = 0;
+                }
               },
             ),
           ),
@@ -80,11 +104,27 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+
+                bool correctAnswer = questions[questionNumber].questionAnswer;
+
+                if (!correctAnswer) {
+                  print('user got it wrong');
+                }
+
+                setState(() {
+                  questionNumber++;
+                });
+                // check if the question number reaches the end of the list
+                if (questionNumber == questions.length) {
+                  questionNumber = 0;
+                }
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
